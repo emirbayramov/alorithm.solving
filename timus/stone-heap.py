911546777f56fab4e098fd557f25ca784ca925a6
emirbayramov <demir.donly@yandex.ru>
2021-11-02 21:01:52 +0300

    solution 1005 not accepted

---
+++ timus/stone-heap.py
@@ -0,0 +1,26 @@
+
+n = int(input())
+ds = input().split(' ')
+ns = []
+for d in ds:
+    ns.append(int(d))
+
+m=0
+end = 2**n
+ms=1000000000
+while m<end:
+    i=0
+    a=b=0
+    while i<n:
+        if (1<<i) & m:
+            a=a+ns[i]
+        else:
+            b=b+ns[i]
+        i=i+1
+    x=a-b
+    s = -x if x<0 else x
+    if(ms>s):
+        ms=s
+    m=m+1
+
+print(ms)
